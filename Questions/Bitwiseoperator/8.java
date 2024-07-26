93d8e8fb886bc569362f50f3ac1283f3e365e5a6
Keerthana K V <167847848+KeertahanaKV@users.noreply.github.com>
2024-07-27 00:20:23 +0530

    Create 8.java

---
+++ Questions/Bitwiseoperator/8.java
@@ -0,0 +1,11 @@
+//find the given numebr is power of 2 0r not
+class PascalTriangle
+{
+    public static void main(String args[])
+    {
+       int n=99;
+        boolean result=(n&(n-1))==0;
+        System.out.println(result);
+        
+    }
+}
