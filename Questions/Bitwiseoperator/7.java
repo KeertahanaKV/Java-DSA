ff467db12b41a8f1d996a19221a2412f439ef5d6
Keerthana K V <167847848+KeertahanaKV@users.noreply.github.com>
2024-07-27 00:12:10 +0530

    Create 7.java

---
+++ Questions/Bitwiseoperator/7.java
@@ -0,0 +1,11 @@
+//find the sum of the nth row in the pascal's triangle
+class PascalTriangle
+{
+    public static void main(String args[])
+    {
+       int n=4;
+        System.out.println(1<<(n-1));
+        //System.out.println(Math.pow(2,n-1));
+        
+    }
+}
