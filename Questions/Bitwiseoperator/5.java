11ad0a89b03254bb226bbe561987ec0046fc6300
Keerthana K V <167847848+KeertahanaKV@users.noreply.github.com>
2024-07-26 22:49:45 +0530

    Update 5.java

--- Questions/Bitwiseoperator/5.java
+++ Questions/Bitwiseoperator/5.java
@@ -10,6 +10,14 @@ public class RightmostSetBit {
         // Calculate the position of the isolated bit
         int position = (int) (Math.log(isolatedBit) / Math.log(2)) + 1;
 
+        
+  /*        // Count the position of the isolated bit
+        while (isolatedBit > 1) {
+            isolatedBit >>= 1; // Shift right to move to the next bit
+            position++; */
+        }
+
+
         return position;
     }
 
