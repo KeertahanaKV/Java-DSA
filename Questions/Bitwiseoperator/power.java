e566bca223cbd02f2ffc456719242f8786d46f39
Keerthana K V <167847848+KeertahanaKV@users.noreply.github.com>
2024-07-29 20:40:32 +0530

    Update power.java

--- Questions/Bitwiseoperator/power.java
+++ Questions/Bitwiseoperator/power.java
@@ -21,3 +21,27 @@ class Power {
         System.out.println(ans);
     }
 }
+/* By using recurrsion
+class Power {
+    public static void main(String args[]) {
+        double x = 4.0;
+        long n = 4;
+        System.out.println(pow(x, n));
+    }
+
+    private static double pow(double base, long p) {
+        if (p == 0) return 1.0;
+        if (p == 1) return base;
+        if (p < 0) return pow(1 / base, -p);
+
+        double res = pow(base, p / 2);
+        res *= res; // Square the result for the even case
+        
+        if (p % 2 == 1) {
+            res *= base; // Handle odd case
+        }
+        
+        return res;
+    }
+}
+*/
