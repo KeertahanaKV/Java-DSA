bfbffeda3440ae57f8690623a254a39f9a876637
Keerthana K V <167847848+KeertahanaKV@users.noreply.github.com>
2025-05-13 09:29:23 +0530

    Create LevelOrder.java

---
+++ Trees/LevelOrder.java
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> list = new ArrayList<>();
+        if(root == null){
+            return list;
+        }
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+        while(!q.isEmpty()){
+            List<Integer> subList = new ArrayList<>();
+            int levelSize = q.size();
+            for(int i=0; i<levelSize; i++){
+                if(q.peek().left != null){
+                    q.offer(q.peek().left);
+                }
+                if(q.peek().right != null){
+                    q.offer(q.peek().right);
+                }
+                subList.add(q.poll().val);
+            }
+            list.add(subList);
+        }
+        return list;
+    }
+}
